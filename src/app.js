3746164f9f376622f34a970c9f96334572882713
Amit08510 <actionchaudhari9090@gmail.com>
2025-08-28 02:18:10 +0545

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-// Connect DB
-connectDB();
+// MongoDB connection
+const mongoURI =
+  "mongodb+srv://actionchaudhari9090:Jonatx9090@cluster0.du0xqw8.mongodb.net/Project101";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 // Middlewares
-app.use(cors({
-    origin: ["https://bootstrap-js-zvv5.onrender.com", "https://project-1-backend-cgpd.onrender.com"],
+app.use(
+  cors({
+    origin: ["https://bootstrap-js-zvv5.onrender.com"],
     credentials: true,
-}));
-
+  })
+);
 app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
